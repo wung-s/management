ffcdd161bf1900be8852a1b0861568e2d6dadc62
wung <wungmathing@gmail.com>
2015-09-18 07:42:32 +0530

    add authentication of user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,21 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-
-  layout 'admin_lte_2'
-  # debugger
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  include Pundit
+  layout 'admin_lte_2'
+  
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  	def user_not_authorized
+  		flash.now[:error] = "You must be logged in !"
+  		redirect_to root_path
+  	end
+
+
+  
 end
