025777e41e47121c5e04771cc8235f35d2b1c5e0
wung <wungmathing@gmail.com>
2015-09-21 09:01:21 +0530

    add flash message type

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,10 +10,14 @@ module ApplicationHelper
 		case message_type
 		when 'notice'
 			alert_class = 'success'
+		when 'success'
+			alert_class = 'success'	
 		when 'alert'
 			alert_class = 'danger'
+		when 'danger'
+			alert_class = 'danger'	
 		else
-			alert_class = 'warning'
+			alert_class = 'info'
 		end
 		
 		alert_class
