774d68318142a9cd37375184fd08c3fd1ebc10f9
wung <wungmathing@gmail.com>
2015-10-18 16:44:45 +0530

    fix for search result

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,25 +1,22 @@
 class CoursesController < ApplicationController
+  before_action :set_course, only: [:index, :search]
+
   def index
-    # debugger
-  	@course = Course.new
-    @course.course_associations.new
+  
     @courses = Course.joins('LEFT OUTER JOIN course_associations ON course_associations.course_id = courses.id LEFT OUTER JOIN departments ON course_associations.department_id = departments.id ').select('courses.id, courses.code, courses.name, departments.name as dept_name, course_associations.id as course_association_id, course_associations.semester as semester, course_associations.credit_hour as credit_hour')
-  	# @courses = Course.all
-  	 # debugger
+  
   end
 
   def search
-    @course = Course.new
-    @course.course_associations.new
-  	@courses = Course.joins('LEFT OUTER JOIN course_associations ON course_associations.course_id = courses.id LEFT OUTER JOIN departments ON course_associations.department_id = departments.id ').select('courses.code, courses.name, departments.name as dept_name').where("courses.code like ?", "%#{params[:code]}%")
-  	# debugger
-  	render 'index'
+    
+  	@courses = Course.joins('LEFT OUTER JOIN course_associations ON course_associations.course_id = courses.id LEFT OUTER JOIN departments ON course_associations.department_id = departments.id ').select('courses.id, courses.code, courses.name, departments.name as dept_name, course_associations.id as course_association_id, course_associations.semester as semester, course_associations.credit_hour as credit_hour').where("courses.code like ?", "%#{params[:code]}%")
+    render 'index'
+  
   end
 
   def edit
     @course = Course.includes(:departments, :course_associations).find(params[:id])
-    # @course.course_associations.build
-    
+
   end
 
   def update
@@ -27,11 +24,11 @@ class CoursesController < ApplicationController
     @course = Course.find(params[:id])
     
     if @course.update(course_params)
-      # debugger
+ 
       flash.now[:success] = "Update successful"
       redirect_to courses_path
     else 
-      # debugger
+ 
       flash.now[:warning] = "Update unsuccessful"
       redirect_to 'edit'  
     end  
@@ -61,6 +58,11 @@ class CoursesController < ApplicationController
     def course_params
       params.require(:course).permit(:code, :name, course_associations_attributes: [:id, :department_id, :credit_hour, :semester])
       # params.require(:course).permit(:code, :name, course_associations_attributes: [ :department_id, :credit_hour, :semester])
+    end
+
+    def set_course
+      @course = Course.new
+      @course.course_associations.new
     end
 
 end
