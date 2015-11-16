c9ae02426a770baca9963e3ac5f9c23c9380d333
wung <wungmathing@gmail.com>
2015-11-16 23:11:59 +0530

    change root of the app

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'marks#index'
+  #root 'marks#index'
+  root 'students#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
