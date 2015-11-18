4216e7bca7a2bc47eec94879a6cd8f5bb7c3a0d7
wung <wungmathing@gmail.com>
2015-11-18 07:14:36 +0530

    validation: state_id is validated only if country_id is for India

--- app/models/student.rb
+++ app/models/student.rb
@@ -6,6 +6,7 @@ class Student < ActiveRecord::Base
 	#set the no of entries to be retrieved using will_paginate
 	self.per_page = 10
 
+
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 
 	validates :first_name, presence: true
@@ -17,8 +18,18 @@ class Student < ActiveRecord::Base
 							 numericality:  {only_integer: true, greater_than_or_equal_to: 0}
 	validates :department_id, presence: true,
 							 numericality:  {only_integer: true, greater_than_or_equal_to: 0}
+
+
 	validates :country_id, presence: true,
 							 numericality:  {only_integer: true, greater_than_or_equal_to: 0}
+    validates :state_id, presence: true,
+    					 numericality:  {only_integer: true, greater_than_or_equal_to: 0},
+    					 if: :country_is_india?
+
+    def country_is_india?
+    	return true if country_id == 1
+    	false
+    end
 
 	def generate_regno(department_id)
 		#the generated length would be 1+3+4+5 = 13 characters
