6a6ae3d9eb1db7d166c60449ad0f76a6438661a5
wung <wungmathing@gmail.com>
2015-11-26 15:15:41 +0530

    remove seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-  GradePoint.create(grade: 'O', point: 7)
-  GradePoint.create(grade: 'A', point: 6)
-  GradePoint.create(grade: 'B', point: 5)
-  GradePoint.create(grade: 'C', point: 4)
-  GradePoint.create(grade: 'D', point: 3)
-  GradePoint.create(grade: 'E', point: 2)
-  GradePoint.create(grade: 'F', point: 0)
 
