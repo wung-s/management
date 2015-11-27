668d78038bbce8ee6600e8093a5de31274f62472
wung <wungmathing@gmail.com>
2015-11-27 08:41:29 +0530

    code clean up and indentation fix

--- app/controllers/marks_controller.rb
+++ app/controllers/marks_controller.rb
@@ -13,51 +13,47 @@ class MarksController < ApplicationController
   end
 
   def search
-  	@students = Student.where("regno like ?", "%#{params[:regno]}%")
-  	render 'index'
+    @students = Student.where("regno like ?", "%#{params[:regno]}%")
+    render 'index'
   end
 
   def import
-  	# debugger
-  	#if params[:file].original_filename =~ /.*\.xls|(xs)$/i  then
+    # debugger
+    #if params[:file].original_filename =~ /.*\.xls|(xs)$/i  then
     # debugger
     flash_msg_elements = Array.new
-    
+
     if params[:course][:id].blank?
       flash_msg_elements.push('Course')
     end
     if params[:department][:id].blank?
       flash_msg_elements.push('Department')
     end
-    # debugger
+
     if flash_msg_elements.empty? then
       if params[:file].nil?
         flash_msg_elements.push('file')
       elsif params[:file].original_filename =~ /.*\.xlsx$/i  then
-      		#process the imported file file
-         
-      		import_state = Mark.import(params[:file], params[:course][:id], params[:department][:id])
-      		if  import_state == false then
-      			flash[:error] = "File is not in the proper format"
-		elsif import_state == 'no association'
-			flash[:error] = "The selected department does not offer the selected course"
-      		elsif import_state.empty?
-      			flash[:success] = "All records have been inserted/updated successfully"
-      		else
-      			flash[:warning] = "There was an error in the following rows: " + import_state.join(", ")
-      		end
-      		
+        #process the imported file file
+
+        import_state = Mark.import(params[:file], params[:course][:id], params[:department][:id])
+        if  import_state == false then
+          flash[:error] = "File is not in the proper format"
+        elsif import_state == 'no association'
+          flash[:error] = "The selected department does not offer the selected course"
+        elsif import_state.empty?
+          flash[:success] = "All records have been inserted/updated successfully"
+        else
+          flash[:warning] = "There was an error in the following rows: " + import_state.join(", ")
+        end
+
       else
-      		flash.now[:error] = "File type is not supported"
+        flash.now[:error] = "File type is not supported"
       end
     else
-        flash.now[:error] = "Please ensure you select the following: " + flash_msg_elements.join(", ")
+      flash.now[:error] = "Please ensure you select the following: " + flash_msg_elements.join(", ")
     end
-    # alert = 'warning testted'    
-  	# debugger
-  	#redirect_to 'marks/index'
-  	# render action: :index
-	redirect_to action: :index
+    redirect_to action: :index
   end
 
   def generate_marksheet
@@ -65,25 +61,16 @@ class MarksController < ApplicationController
   end
 
   def genpdf
-    # @user = User.first
-    # debugger
     respond_to do |format|
       format.html
-      # debugger
       format.pdf do 
-        # pdf = UserPdf.new(@user)
         pdf = MarksheetPdf.new
-        # pdf.text "hello world"
-        
         send_data pdf.render, filename: "test_file",
-                            type: "application/pdf",
-                            disposition: "inline"
-        
+          type: "application/pdf",
+          disposition: "inline"
+
         #  send_data pdf.render
       end
-
     end
   end
-
-
 end
